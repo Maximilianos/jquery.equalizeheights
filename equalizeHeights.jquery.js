44e1fb371720c9fe119752417fa68ef6b274e31c
Max G J Panas <m@maxpanas.com>
2013-06-25 21:44:24 +0300

    Added License Agreement and Tidied up code

--- equalizeHeights.jquery.js
+++ equalizeHeights.jquery.js
@@ -1,23 +1,51 @@
-(function ($, eh) {  
-  $.fn[eh] = function () {
-    if (this.length > 1) {
-      var that = $(this);
-      $(window).resize(function () {
-        var maxheight = 0;
-        that.each(function () {
-          var me = $(this),
-            height = me.css({min-height: ''}); // reset box height to natural
-          // solution for box-sizing + .height() bug in jQuery:
-          height = me.css('box-sizing') == 'content-box' ? height.height() : height.outerHeight();          
-          if (height > maxheight) maxheight = height;
-        }).css({min-height: maxheight});
-      }).resize();
-    }
-  };
-}(jQuery, 'equalizeHeights'));
+/*!
+ * jQuery Equalize Heights Plugin - v 1.0 - 25/05/2013
+ * by Max G J Panas (m@maxpanas.com), http://maxpanas.com
+ *
+ * Copyright (c) 2013 Max G J Panas
+ * Dual licenced under MIT (http://opensource.org/licenses/MIT) and GPL (http://www.gnu.org/licenses/gpl.html) licences
+ */
 
-// example usage:
+// * Examples of Use:
+//
+//  * simple:
+//
+//    $('.equal').equalizeHeights();
+//
+//  * or on resize for RWD (Responsive Web Design):
+//
+//    var el = $('.equal');
+//    $(window).resize(function () {
+//      el.equalizeHeights();
+//    }).resize();
+
 (function ($) {
-  $('.eq1').equalizeHeights();
-  $('.eq2').equalizeHeights();
+
+  $.fn.equalizeHeights = function () {
+
+    if (this.length > 1) { // no point in running this on fewer than two items
+
+      var maxheight = 0, css;
+
+      $(this).each(function () {
+        // reset element height to natural in order to get real height values
+        var height = $(this).css({'min-height': '', height: ''});
+
+        // resolve issue of using box-sizing: border-box || padding-box when getting and setting $(el).height(val);
+        height = $(this).css({'box-sizing'}) == 'content-box' ? height.height() : height.outerHeight();
+
+        if (height > maxheight) maxheight = height;
+      });
+
+      css = {'min-height': maxheight};
+
+      // IE6 support - IE6 does not support min-height, so set height as well
+      if ($.browser.msie && $.browser.version == 6.0) { css.height = maxheight }
+
+      $(this).css(css);
+
+    }
+
+  };
+
 }(jQuery));
